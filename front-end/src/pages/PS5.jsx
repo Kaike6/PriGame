adcc5cb2e64add21bfe7aef83b1308088ef5b817
renegador42-lab <renegador42@gmail.com>
2026-03-24 18:27:18 -0300

    update

--- front-end/src/pages/PS5.jsx
+++ front-end/src/pages/PS5.jsx
@@ -1,5 +1,5 @@
+import "../style/home.css"
 import "../style/cards.css"
-import "../style/ps5.css"
 
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
@@ -16,25 +16,17 @@ import estojoPs5 from "../assets/estojoPs5.jpg"
 import foneBluttof from "../assets/foneBluttof.jpg"
 import fonefio from "../assets/fonefio.jpg"
 
-import Button from "../components/Button"
-import Input from "../components/Input"
 import Jogos from "../components/Jogos"
-import Label from "../components/Label"
+import Pesquisa from "../components/Pesquisa"
 
 function PS5() {
 
   const [pesquisa, setPesquisa] = useState("")
-  const [texto, setTexto] = useState("")
-
   const navigate = useNavigate()
 
   function comprar(produto) {
     localStorage.setItem("produto", JSON.stringify(produto))
     navigate("/Compras")
-  }
-
-  function pesquisar() {
-    setPesquisa(texto)
   }
 
   const produtosPS5 = [
@@ -43,97 +35,58 @@ function PS5() {
     { nome: "PS3", img: Ps3, preco: 1200, desc: "Playstation 3" },
     { nome: "PS4", img: Ps4, preco: 2500, desc: "Playstation 4" },
     { nome: "PS5", img: Ps5, preco: 4500, desc: "Playstation 5" },
-  {
-    nome: "Capa Proteção",
-    img: capaProtecao,
-    preco: 100,
-    desc: "Capa proteção controle Playstation 5"
-  },
 
-  {
-    nome: "Carregador PS5",
-    img: caregadorPs5,
-    preco: 150,
-    desc: "Carregador para controle PS5"
-  },
-
-  {
-    nome: "Estojo PS5",
-    img: estojoPs5,
-    preco: 80,
-    desc: "Estojo para guardar controle"
-  },
-
-  {
-    nome: "Fone Bluetooth",
-    img: foneBluttof,
-    preco: 200,
-    desc: "Fone sem fio gamer"
-  },
-
-  {
-    nome: "Fone com fio",
-    img: fonefio,
-    preco: 90,
-    desc: "Fone gamer com fio"
-  }
-
-]
-  
-
-  const produtosInvisivel = [
-    {
-      nome: "capaProtecao",
-      img: capaProtecao,
-      preco: 100,
-      desc: "Capa proteção controle Playstation 5"
-    }
+    { nome: "Capa Proteção", img: capaProtecao, preco: 100, desc: "Capa controle" },
+    { nome: "Carregador", img: caregadorPs5, preco: 150, desc: "Carregador PS5" },
+    { nome: "Estojo", img: estojoPs5, preco: 80, desc: "Estojo controle" },
+    { nome: "Fone Bluetooth", img: foneBluttof, preco: 200, desc: "Fone gamer" },
+    { nome: "Fone com fio", img: fonefio, preco: 90, desc: "Fone gamer" }
   ]
 
-  const todosProdutos = [...produtosPS5, ...produtosInvisivel]
+  const produtospesquisar = [
+    { nome: "PS4", img: Ps4, preco: 2500, desc: "Playstation 4" },
+    { nome: "PS5", img: Ps5, preco: 4500, desc: "Playstation 5" },
 
-  const produtosFiltrados = todosProdutos.filter(p =>
+  ]
+
+  const produtosFiltrados = produtosPS5 && produtospesquisar.filter(p =>
     p.nome.toLowerCase().includes(pesquisa.toLowerCase())
   )
 
   return (
-    <div className="containerPs5">
 
-      <Label>Pesquisar produtos</Label>
+    <main className="home">
 
-      {/* INPUT + BOTÃO */}
-      <div className="pesquisa">
-
-        <Input
-          type="text"
-          placeholder="Pesquisar..."
-          onChange={(e) => setTexto(e.target.value)}
-        />
-
-        <Button onClick={pesquisar}>
-          Pesquisar
-        </Button>
-
+      <h1>CATÁLOGO PS5</h1>
+            <div className="pesquisa">
+        <Pesquisa setPesquisa={setPesquisa} />
       </div>
-
-
-      {pesquisa && (
+            {pesquisa && (
         <>
-          <h3>Resultado</h3>
+          <h2>RESULTADO</h2>
 
-          <div className="produtosinvisivel">
+          <div className="jogosInvisivel">
             <Jogos jogos={produtosFiltrados} comprar={comprar} />
           </div>
         </>
       )}
 
-      <h2>PRODUTOS MAIS VENDIDOS</h2>
 
-      <div className="produtosPs5">
+      <h2>PRODUTOS</h2>
+
+      <div className="jogos">
         <Jogos jogos={produtosPS5} comprar={comprar} />
       </div>
 
-    </div>
+
+      <h4>PESQUISAR</h4>
+
+
+
+
+
+
+    </main>
   )
 }
 
--- front-end/src/pages/Xbox.jsx
+++ front-end/src/pages/Xbox.jsx
@@ -1,18 +1,124 @@
-import "../style/xbox.css"
+import "../style/home.css"
+import "../style/cards.css"
 
-function Xbox(){
-    return(
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+import Ps1 from "../assets/Ps1.jpg"
+import Ps2 from "../assets/Ps2.jpg"
+import Ps3 from "../assets/Ps3.jpg"
+import Ps4 from "../assets/Ps4.webp"
+import Ps5 from "../assets/Ps5.webp"
+
+import capaProtecao from "../assets/capaProtecao.webp"
+import caregadorPs5 from "../assets/caregadorPs5.png"
+import estojoPs5 from "../assets/estojoPs5.jpg"
+import foneBluttof from "../assets/foneBluttof.jpg"
+import fonefio from "../assets/fonefio.jpg"
+
+import Jogos from "../components/Jogos"
+import Pesquisa from "../components/Pesquisa"
+
+function Xbox() {
+
+  const [pesquisa, setPesquisa] = useState("")
+  const navigate = useNavigate()
+
+  function comprar(produto) {
+    localStorage.setItem("produto", JSON.stringify(produto))
+    navigate("/Compras")
+  }
+
+
+  const produtosPS5 = [
+    { nome: "PS1", img: Ps1, preco: 500, desc: "Playstation 1" },
+    { nome: "PS2", img: Ps2, preco: 700, desc: "Playstation 2" },
+    { nome: "PS3", img: Ps3, preco: 1200, desc: "Playstation 3" },
+    { nome: "PS4", img: Ps4, preco: 2500, desc: "Playstation 4" },
+    { nome: "PS5", img: Ps5, preco: 4500, desc: "Playstation 5" },
+  {
+    nome: "Capa Proteção",
+    img: capaProtecao,
+    preco: 100,
+    desc: "Capa proteção controle Playstation 5"
+  },
+
+  {
+    nome: "Carregador PS5",
+    img: caregadorPs5,
+    preco: 150,
+    desc: "Carregador para controle PS5"
+  },
+
+  {
+    nome: "Estojo PS5",
+    img: estojoPs5,
+    preco: 80,
+    desc: "Estojo para guardar controle"
+  },
+
+  {
+    nome: "Fone Bluetooth",
+    img: foneBluttof,
+    preco: 200,
+    desc: "Fone sem fio gamer"
+  },
+
+  {
+    nome: "Fone com fio",
+    img: fonefio,
+    preco: 90,
+    desc: "Fone gamer com fio"
+  }
+
+]
+
+
+  const produtospesquisar = [
+    { nome: "PS4", img: Ps4, preco: 2500, desc: "Playstation 4" },
+    { nome: "PS5", img: Ps5, preco: 4500, desc: "Playstation 5" },
+
+  ]
+
+  const produtosFiltrados = produtosPS5 && produtospesquisar.filter(p =>
+    p.nome.toLowerCase().includes(pesquisa.toLowerCase())
+  )
+
+  return (
+
+    <main className="home">
+
+      <h1>CATÁLOGO PS5</h1>
+            <div className="pesquisa">
+        <Pesquisa setPesquisa={setPesquisa} />
+      </div>
+            {pesquisa && (
         <>
-        <div className="containerXbox">
-            <div className="Xbox"></div>
-            <div className="Xbox"></div>
-            <div className="Xbox"></div>
-            <div className="Xbox"></div>
-            <div className="Xbox"></div>
-            <div className="Xbox"></div>
-        </div>
+          <h2>RESULTADO</h2>
+
+          <div className="jogosInvisivel">
+            <Jogos jogos={produtosFiltrados} comprar={comprar} />
+          </div>
         </>
-    )
+      )}
+
+
+      <h2>PRODUTOS</h2>
+
+      <div className="jogos">
+        <Jogos jogos={produtosPS5} comprar={comprar} />
+      </div>
+
+
+      <h4>PESQUISAR</h4>
+
+
+
+
+
+
+    </main>
+  )
 }
 
-export default Xbox;
+export default Xbox
